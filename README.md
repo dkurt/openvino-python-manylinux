6649aa0ced30b7988ab615e4437a5fe0505a99f1
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-12-10 22:54:31 +0300

    Build Linux wheels

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# openvino-python-manylinux
+# Python bingings for Intel OpenVINO
+
+This is unofficial version of OpenVINO for Python created with `manylinux2014` toolchain.
+
+Supported devices: CPU
+
+* How to install
+
+    ```bash
+    python3 -m pip install --upgrade pip
+    python3 -m pip install experimental-openvino-python
+    ```
+
+---
+
+* [Changelog](https://github.com/dkurt/openvino-python-manylinux/wiki/Changelog)
---
+++ manylinux/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import os
+from setuptools import setup, Extension
+
+if not 'VERSION' in os.environ:
+    raise Exception('Script usage: VERSION=x.x.x python3 setup.py sdist')
+
+py_version = '{}.{}'.format(sys.version_info[0], sys.version_info[1])
+
+extension_module = Extension(
+    'experimental-openvino-python.extension',
+    sources=[],
+    library_dirs=['/openvino/bin/intel64/Release/lib/'],
+    libraries=['inference_engine'],
+)
+
+setup(name='experimental-openvino-python',
+      version=os.environ['VERSION'],
+      author='Dmitry Kurtaev',
+      url='https://github.com/dkurt/openvino-python-manylinux/',
+      ext_modules=[extension_module],
+      packages=['openvino', 'openvino.inference_engine'],
+      package_dir={'openvino': '/openvino/bin/intel64/Release/lib/python_api/python{}/openvino'.format(py_version)},
+      package_data={
+          'openvino': [
+              '/openvino/bin/intel64/Release/lib/python_api/python{}/openvino/inference_engine/ie_api.so'.format(py_version),
+              '/openvino/bin/intel64/Release/lib/python_api/python{}/openvino/inference_engine/constants.so'.format(py_version),
+          ],
+      },
+      data_files=[('../../experimental_openvino_python.libs', [
+        '/openvino/bin/intel64/Release/lib/plugins.xml',
+        '/openvino/bin/intel64/Release/lib/libinference_engine_ir_reader.so',
+        '/openvino/bin/intel64/Release/lib/libinference_engine_onnx_reader.so',
+        '/openvino/bin/intel64/Release/lib/libinference_engine_preproc.so',
+        '/openvino/bin/intel64/Release/lib/libMKLDNNPlugin.so',
+        # '/openvino/bin/intel64/Release/lib/libHeteroPlugin.so',
+        # '/openvino/bin/intel64/Release/lib/libclDNNPlugin.so',
+        # '/openvino/bin/intel64/Release/lib/libGNAPlugin.so',
+        # '/openvino/bin/intel64/Release/lib/libMultiDevicePlugin.so',
+      ])],
+      install_requires=['numpy'],
+)
